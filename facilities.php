eea3ff036be97c12d99984bd697f662f1e611d8f
HungVu0112 <pcsolaggy123@gmail.com>
2023-09-01 23:39:21 +0700

    [Page] Facilities

---
+++ facilities.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css">
+    <?php require('inc/links.php') ?>
+    <title>PC Hotel - Facilities</title>
+    <style>
+        .pop:hover {
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.3s;
+        }
+    </style>
+</head>
+<body class="bg-light">
+
+    <?php require('inc/header.php') ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h_font text-center">OUR FACILITIES</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam fugit eius at, deserunt dicta qui facilis illum. <br> Est quia repellat provident dolorem fugiat rerum id hic! Provident, quia repellat.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/IMG_43553.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eum accusamus cupiditate sit! Eaque error itaque numquam, sed illo neque. Incidunt blanditiis eius deserunt laudantium earum? Necessitatibus laborum doloremque eos!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/IMG_27079.svg" width="40px">
+                        <h5 class="m-0 ms-3">Heater</h5>
+                    </div>    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eum accusamus cupiditate sit! Eaque error itaque numquam, sed illo neque. Incidunt blanditiis eius deserunt laudantium earum? Necessitatibus laborum doloremque eos!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/IMG_41622.svg" width="40px">
+                        <h5 class="m-0 ms-3">Television</h5>
+                    </div>    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eum accusamus cupiditate sit! Eaque error itaque numquam, sed illo neque. Incidunt blanditiis eius deserunt laudantium earum? Necessitatibus laborum doloremque eos!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/IMG_49949.svg" width="40px">
+                        <h5 class="m-0 ms-3">Air Conditioner</h5>
+                    </div>    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eum accusamus cupiditate sit! Eaque error itaque numquam, sed illo neque. Incidunt blanditiis eius deserunt laudantium earum? Necessitatibus laborum doloremque eos!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/IMG_96423.svg" width="40px">
+                        <h5 class="m-0 ms-3">Radio</h5>
+                    </div>    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eum accusamus cupiditate sit! Eaque error itaque numquam, sed illo neque. Incidunt blanditiis eius deserunt laudantium earum? Necessitatibus laborum doloremque eos!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/IMG_47816.svg" width="40px">
+                        <h5 class="m-0 ms-3">Massage</h5>
+                    </div>    
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eum accusamus cupiditate sit! Eaque error itaque numquam, sed illo neque. Incidunt blanditiis eius deserunt laudantium earum? Necessitatibus laborum doloremque eos!</p>
+                </div>
+            </div> 
+        </div>
+    </div>
+
+    <?php require('inc/footer.php') ?>
+</body>
+</html>
---
+++ inc/footer.php
@@ -0,0 +1,32 @@
+<div class="container-fluid bg-white mt-5">
+        <div class="row">
+            <div class="col-lg-4 p-4">
+                <h3 class="h_font fw-bold fs-3 mb-2">PC Hotel</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est vitae neque deleniti animi, dolorem nobis blanditiis excepturi atque soluta quo molestias eum expedita, itaque eius et a asperiores voluptas?</p>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="#" class="d-block mb-2 text-dark text-decoration-none">Home</a>
+                <a href="#" class="d-block mb-2 text-dark text-decoration-none">Rooms</a>
+                <a href="#" class="d-block mb-2 text-dark text-decoration-none">Facilities</a>
+                <a href="#" class="d-block mb-2 text-dark text-decoration-none">Contact us</a>
+                <a href="#" class="d-block mb-2 text-dark text-decoration-none">About</a>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Follow us</h5>
+                <a href="#" class="d-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-twitter"></i> Twitter
+                </a>
+                <a href="#" class="d-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-facebook"></i> Facebook
+                </a>
+                <a href="#" class="d-block text-dark text-decoration-none">
+                    <i class="bi bi-instagram"></i> Instagram
+                </a>
+            </div>
+        </div>
+</div>
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and developed by Pcsolaggy</h6>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
