dcbec268e6f5d004fd6174811c5782a08cb3225c
HungVu0112 <pcsolaggy123@gmail.com>
2023-09-01 22:48:29 +0700

    [Interface] Reach us

--- index.php
+++ index.php
@@ -487,6 +487,51 @@
         </div>
     </div>
 
+    <!-- Reach us -->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h_font">REACH US</h2>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100" height="320" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59658.044491247914!2d106.65754685191587!3d20.84673192365151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7af39e3f1ad3%3A0xa5ffc85ff87a07e8!2zSOG6o2kgUGjDsm5nLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1693582476190!5m2!1svi!2s" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Call us</h5>
+                    <a href="tel: 0948382856" class="d-inline-block mb-2 text-decoration-none text-dark ">
+                        <i class="bi bi-telephone-fill"></i> 0948382856
+                    </a>
+                    <br>
+                    <a href="tel: 0948382856" class="d-inline-block text-decoration-none text-dark ">
+                        <i class="bi bi-telephone-fill"></i> 0948382856
+                    </a>
+                </div>
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Follow us</h5>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-twitter"></i> Twitter
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-facebook"></i> Facebook
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-instagram"></i> Instagram
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
     <br><br><br>
     <br><br><br>
