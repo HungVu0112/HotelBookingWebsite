48a21bbc4bf71414a49573ac465aac339479bad7
HungVu0112 <pcsolaggy123@gmail.com>
2023-09-01 22:06:38 +0700

    [Interface] Facilities section

--- index.php
+++ index.php
@@ -404,6 +404,36 @@
         </div>
     </div>
 
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h_font">OUR FACILITIES</h2>
+
+    <div class="container">
+        <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+                <img src="images/facilities/IMG_43553.svg" width="80px" >
+                <h5 class="mt-3">Wifi</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+                <img src="images/facilities/IMG_27079.svg" width="80px" >
+                <h5 class="mt-3">Heater</h5>
+            </div>  
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+                <img src="images/facilities/IMG_41622.svg" width="80px" >
+                <h5 class="mt-3">Television</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+                <img src="images/facilities/IMG_96423.svg" width="80px" >
+                <h5 class="mt-3">Radio</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+                <img src="images/facilities/IMG_49949.svg" width="80px" >
+                <h5 class="mt-3">Air Conditioner</h5>
+            </div>
+            <div class="col-lg-12 text-center mt-5">
+                <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+            </div>
+        </div>
+    </div>
+
     <br><br><br>
     <br><br><br>
 
